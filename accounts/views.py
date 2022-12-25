e4761340b7481d2a2d4ba570181119977cadaf74
ryayar <89771789+ryayar@users.noreply.github.com>
2022-12-25 16:11:41 +0400

    Работа над формой авторизации

---
+++ accounts/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+from accounts.forms import LoginForm
+
+
+class LoginView(View):
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username=cd['username'],
+                password=cd['password']
+            )
+            if user is None:
+                return HttpResponse('Неправильный логин и/или пароль')
+            if not user.is_active:
+                return HttpResponse('Ваш аккаунт заблокирован')
+            login(request, user)
+            return HttpResponse('Добро пожаловать! Успешный вход')
+        return render(request, 'accounts/login.html', {'form': form})
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm()
+        return render(request, 'accounts/login.html', {'form': form})
